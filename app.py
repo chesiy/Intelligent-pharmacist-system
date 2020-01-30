5033382dbfb3b6f118b72b95bf3c0f5ea4eafd04
chesiy <49095156+chesiy@users.noreply.github.com>
2020-01-30 22:35:02 +0800

    upd

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 from flask import request,redirect,url_for
 from flask_wtf import FlaskForm
-from my_neo import get_drug
+from my_neo import get_drug,get_drug_from_product,get_drug_from_symptom
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 
@@ -15,8 +15,11 @@ class symptomForm(FlaskForm):
 class scriptForm(FlaskForm):
     sympt_name2=StringField('病症：',validators=[DataRequired()])
     product1_name=StringField('药品1:',validators=[DataRequired()])
-    product2_name=StringField('药品2:',validators=[DataRequired()])
-    product3_name = StringField('药品3:', validators=[DataRequired()])
+    product2_name=StringField('药品2:')
+    product3_name = StringField('药品3:')
+    product1_useage=StringField('用法用量：',validators=[DataRequired()])
+    product2_useage = StringField('用法用量：')
+    product3_useage = StringField('用法用量：')
     submit2=SubmitField('提交处方')
 
 @app.route('/',methods=['POST','GET'])
@@ -36,21 +39,37 @@ def checkScript():
     script_form = scriptForm()
     if request.method=='POST':
         symp2_name = script_form.sympt_name2.data
-        drug1_name =script_form.drug1_name.data
-        drug2_name=script_form.drug2_name.data
+        product_name=[]
+        product_use=[]
+        product_name.append(script_form.product1_name.data)
+        product_name.append(script_form.product2_name.data)
+        product_name.append(script_form.product3_name.data)
+        product_use.append(script_form.product1_useage.data)
+        product_use.append(script_form.product2_useage.data)
+        product_use.append(script_form.product3_useage.data)
 
-        SuitableDrugs=get_drug(symp2_name)
-        if (drug1_name in SuitableDrugs) and (drug2_name in SuitableDrugs):
-            return 'Success'
-        else:
-            return 'Fail'
+        product_num=0
+        for onename in product_name:
+            if onename!='':
+                product_num= product_num+1
+
+        SuitableDrugs=get_drug_from_symptom(symp2_name)
+
+        cnt=0
+        for oneproductname in product_name:
+            for onedrug in get_drug_from_product(oneproductname):
+                if onedrug in SuitableDrugs:
+                    cnt=cnt+1
+
+        if cnt==product_num:
+            return 'success'
+        return 'fail'
 
     return render_template('Check_script.html',scri_form=script_form)
 
 @app.route('/drugs/?<string:symp_name>')
 def getdrug(symp_name):
     drug_result,product_result = get_drug(symp_name)
-    print('*****************')
     return render_template('suitable_drugs.html', drug_result=drug_result,product_result=product_result)
 
 if __name__  ==  '__main__':
--- my_neo.py
+++ my_neo.py
@@ -10,7 +10,7 @@ def get_drug_product(drugname):  #由药物名查询药品
         result_product.append('药品名称：'+results['Product.name']+' 制剂规格：'+results['Product.size'])
     return result_product
 
-def get_drug(symptomname):  # 由疾病名称查询对症药物
+def get_drug(symptomname):  # 由疾病名称查询对症药物和药品(task1 uses only)
     graph = Graph("bolt://localhost:7687", username="neo4j", password="123456")
     # nodematcher=NodeMatcher(graph)
     # relamatcher=RelationshipMatcher(graph)
@@ -28,3 +28,27 @@ def get_drug(symptomname):  # 由疾病名称查询对症药物
         products[oneresult['drug.name']]=get_drug_product(oneresult['drug.name'])
     print(products)
     return result_drug,products
+
+def get_drug_from_symptom(symptomname): #由疾病名称查询对症药物
+    graph = Graph("bolt://localhost:7687", username="neo4j", password="123456")
+    str = 'MATCH a=(symptom{name:\'' + symptomname + '\'})-[:可使用]->(drug) return drug.name'
+    result_list = list(graph.run(str).data())
+    result_drug = []
+    for oneresult in result_list:
+        result_drug.append(oneresult['drug.name'])
+    return result_drug
+
+def get_drug_from_product(productname): #由药品查药物
+    graph = Graph("bolt://localhost:7687", username="neo4j", password="123456")
+    str = 'MATCH a=(Product{name:\'' + productname + '\'})-[:对应药物]->(drug) return drug.name'
+    result_list = list(graph.run(str).data())
+    result_drug = []
+    for oneresult in result_list:
+        result_drug.append(oneresult['drug.name'])
+    return result_drug
+
+def get_usage_from_product(productname): #由药品查用法
+    graph = Graph("bolt://localhost:7687", username="neo4j", password="123456")
+    str = 'MATCH a=(Product{name:\'' + productname + '\'})-[:用法]->(Usage) return Usage.frequency,Usage.useage,Usage.consumption,Usage.notes'
+    result_list = list(graph.run(str).data())
+    print(result_list)
