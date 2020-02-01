1c280309727183d09be3a671ecf99cb53253b344
chesiy <49095156+chesiy@users.noreply.github.com>
2020-02-01 16:17:51 +0800

    Help!!!!

--- app.py
+++ app.py
@@ -14,24 +14,16 @@ class symptomForm(FlaskForm):
 
 class scriptForm(FlaskForm):
     sympt_name2=StringField('病症：',validators=[DataRequired()])
+    product_num=IntegerField('药品数量',render_kw={'size':"2"})
     people_age = SelectField('年龄段', choices=[(1,'成人'), (2,'儿童')], validators=[DataRequired('请选择年龄段')])
-    specific_people=SelectField('特殊人群',choices=[(1,'哺乳期妇女'),(2,'过敏患者'),(3,'妊娠期妇女'),(4,'老年人'),(5,'未成年人'),(6,'未成熟儿'),(7,'新生儿'),(8,'孕妇'),(9,'早产儿')])
+    specific_people=SelectField('特殊人群',choices=[(0,'否'),(1,'哺乳期妇女'),(2,'过敏患者'),(3,'妊娠期妇女'),(4,'老年人'),(5,'未成年人'),(6,'未成熟儿'),(7,'新生儿'),(8,'孕妇'),(9,'早产儿')])
     diseases = StringField('基础疾病')
-    product1_name=StringField('药品1:',validators=[DataRequired()])
-    product2_name=StringField('药品2:')
-    product3_name = StringField('药品3:')
-    frequency1 = IntegerField('一日',render_kw={'size':"2"})
-    frequency2 = IntegerField('一日',render_kw={'size':"2"})
-    frequency3 = IntegerField('一日',render_kw={'size':"2"})
-    fre1=IntegerField('每几小时一次',render_kw={'size':"2"})
-    fre2 = IntegerField('每几小时一次',render_kw={'size':"2"})
-    fre3 = IntegerField('每几小时一次',render_kw={'size':"2"})
-    amount1=IntegerField('一次',render_kw={'size':"2"})
-    amount2=IntegerField('一次',render_kw={'size':"2"})
-    amount3 = IntegerField('一次',render_kw={'size':"2"})
-    usage1=SelectField('用法',choices=[(1,'口服'),(2,'注射')],validators=[DataRequired('请选择用法')])
-    usage2=SelectField('用法',choices=[(1,'口服'),(2,'注射')])
-    usage3 = SelectField('用法',choices=[(1,'口服'),(2,'注射')])
+    product_name=[StringField('药品:')]*5
+    frequency=[IntegerField('一日',render_kw={'size':"2"})]*5
+    fre=[IntegerField('每几小时一次',render_kw={'size':"2"})]*5
+    amount1=[IntegerField('一次',render_kw={'size':"2"})]*5 #一日几次的
+    amount2 = [IntegerField('一次', render_kw={'size': "2"})] * 5  #每几小时一次的
+    usage=[SelectField('用法',choices=[(1,'口服'),(2,'注射')])]*5
 
     submit2=SubmitField('提交处方')
 
@@ -52,24 +44,30 @@ def checkScript():
     script_form = scriptForm()
     if request.method=='POST':
         symp2_name = script_form.sympt_name2.data
+        true_product_num=script_form.product_num
         product_name=[]
-        product_use=[]
+        product_use=[] #口服/注射
         product_name.append(script_form.product1_name.data)
         product_name.append(script_form.product2_name.data)
         product_name.append(script_form.product3_name.data)
-        product_use.append(script_form.product1_useage.data)
-        product_use.append(script_form.product2_useage.data)
-        product_use.append(script_form.product3_useage.data)
-
+        product_use.append(script_form.usage1.data)
+        product_use.append(script_form.usage2.data)
+        product_use.append(script_form.usage3.data)
+        print('@@@@@@@@@@@@@@@@@@@@@@@')
+        print(product_use)
         product_num=0
         for onename in product_name:
             if onename!='':
                 product_num= product_num+1
+        if product_num!=true_product_num:
+            return '验证失败，填写的处方中药品数目与实际提交的药品数目不符'
 
         SuitableDrugs=get_drug_from_symptom(symp2_name)
 
         cnt=0
         for oneproductname in product_name:
+            if product_use[oneproductname.index()]== 1:#口服
+                product_frequency=script_form.frequency1
             for onedrug in get_drug_from_product(oneproductname):
                 if onedrug in SuitableDrugs:
                     cnt=cnt+1
@@ -86,4 +84,4 @@ def getdrug(symp_name):
     return render_template('suitable_drugs.html', drug_result=drug_result,product_result=product_result)
 
 if __name__  ==  '__main__':
-    app.run()
+    app.run(debug=True)
